eb407fe4acdc6c96db979f5f60391091fce89ce2
dykoffi <dykoffi2000@gmail.com>
2022-12-18 18:19:29 +0000

    Update chart

--- pkg.sh
+++ pkg.sh
@@ -1,7 +1,3 @@
-# name=dykoffi
-# keyring=~/.gnupg/pubring.kbx
-repo_dir=~/Projets/helm
+repo_dir=/home/dy/Projets/helm
 repo_url=https://data354.github.io/helm/
-
-# helm package --sign --key $name --keyring $keyring -d ${repo_dir}/monitoring-stack .
 helm repo index $repo_dir --url $repo_url
