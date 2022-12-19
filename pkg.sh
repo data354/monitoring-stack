419c4346ea09e42f348efb21ca2ecc813f0f4da6
dykoffi <dykoffi2000@gmail.com>
2022-12-19 00:08:10 +0000

    Fix git repo cmd

--- pkg.sh
+++ pkg.sh
@@ -1,3 +1,5 @@
-repo_dir=/home/dy/Projets/helm
+repo_dir=~/Projets/helm
 repo_url=https://data354.github.io/helm/
+
+helm package . -d ~/Projets/helm/monitoring-stack
 helm repo index $repo_dir --url $repo_url
